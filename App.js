509a7c001c39d66c95fadafa674754df1e19d2da
Cristiano Ribeiro <crysmar1@outlook.com>
2024-04-22 08:54:29 -0300

    Commit initial appPiBic

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>Meu Primeiro APP com React!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  red: {
+    color: 'red',
+    fontSize: 30
+  }
 });
