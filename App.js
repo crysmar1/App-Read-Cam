3211dfeab72bd0e3d65d3b63975f459584302856
Cristiano Ribeiro <crysmar1@outlook.com>
2024-04-29 23:18:38 -0300

    implementacao leitor qrcode

--- App.js
+++ App.js
@@ -1,24 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+// Resolver as permissões
+import { check, request, PERMISSIONS, RESULTS } from '@react-native-permissions';
 
-export default function App() {
+const App = () => {
+  const [qrCodes, setQrCodes] = useState([]);
+
+  useEffect(() => {
+    const checkCameraPermission = async () => {
+      // Verifica se a permissão de câmera foi concedida
+      const permissionResult = await check(PERMISSIONS.IOS.CAMERA);
+
+      if (permissionResult === RESULTS.DENIED) {
+        // Se a permissão foi negada, solicita ao usuário
+        const requestResult = await request(PERMISSIONS.IOS.CAMERA);
+
+        if (requestResult !== RESULTS.GRANTED) {
+          // Se a permissão não foi concedida, exibe um alerta
+          Alert.alert(
+            'Permissão negada',
+            'Você precisa permitir o acesso à câmera para usar este aplicativo.'
+          );
+        }
+      }
+    };
+
+    checkCameraPermission();
+  }, []);
+
+  const handleReadQrCode = ({ data }) => {
+    // Adiciona o QR code lido à lista de QR codes
+    setQrCodes(prevQrCodes => [...prevQrCodes, data]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.red}>Meu Primeiro APP com React!!!</Text>
-      <StatusBar style="auto" />
+      <QRCodeScanner
+        onRead={handleReadQrCode}
+        cameraStyle={styles.cameraContainer}
+        cameraProps={{ flashMode: RNCamera.Constants.FlashMode.auto }}
+      />
+      <View style={styles.qrCodesContainer}>
+        {qrCodes.map((qrCode, index) => (
+          <Text key={index} style={styles.qrCodeText}>{qrCode}</Text>
+        ))}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  red: {
-    color: 'red',
-    fontSize: 30
-  }
+  cameraContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  qrCodesContainer: {
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    alignSelf: 'stretch',
+  },
+  qrCodeText: {
+    color: 'white',
+    marginBottom: 10,
+    fontSize: 16,
+  },
 });
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Aplicação iniciada com React Native, nodeJs e expo.
+Aplicação iniciada com React e React Native, nodeJs e expo.
+
+Uso das bibliotecas react-native-qrcode-scanner e react-native-camera
